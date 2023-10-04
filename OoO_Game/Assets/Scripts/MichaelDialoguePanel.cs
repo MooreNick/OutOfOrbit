e9ba382bae19d3c1baff805113cef3dbd96a772f
estauss1 <estauss23@gmail.com>
2023-10-04 15:44:43 -0400

    added dialogue with npc that has a type out affect most newly added files are from importing text mesh pro

--- OoO_Game/Assets/Scripts/MichaelDialoguePanel.cs
+++ OoO_Game/Assets/Scripts/MichaelDialoguePanel.cs
@@ -1,12 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MichaelDialoguePanel : MonoBehaviour
 {
+    private TextMeshProUGUI dialogueTM;
+    public float secsBetweenCharsTyped;
+    public string michaelGreeting;
+
     private void Start()
     {
+        dialogueTM = GetComponentInChildren<TextMeshProUGUI>();
         gameObject.SetActive(false); //not showing on game start
+    }
+
+    public void startDialogue()
+    {
+        StartCoroutine(typeDialogue(michaelGreeting));
+    }
+
+    IEnumerator typeDialogue(string lineOfDialogue)
+    {
+        if(dialogueTM != null)
+        {
+            foreach(char c in lineOfDialogue)
+            {
+                dialogueTM.text += c;
+                yield return new WaitForSeconds(secsBetweenCharsTyped);
+            }
+        }
     }
 
     public void toggleActive()
