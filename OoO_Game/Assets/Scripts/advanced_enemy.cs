bf1ac9807df5ee18c712aed661f3430928ccc166
Nicholas Moore <nmoore22@kent.edu>
2023-10-12 11:08:17 -0400

    added prefab for basic and advanced enemy, created layer for player/enemies
    Layer is set at 20

---
+++ OoO_Game/Assets/Scripts/advanced_enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class advanced_enemy : MonoBehaviour
+{
+
+    public int health = 300;
+    public int speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
